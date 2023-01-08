3942bdfc6f9e1c898631c4096f5f4be8d00e7405
Ryder Belserion <no-reply@ryderbelserion.com>
2023-01-08 00:44:51 -0500

    [ci-skip] Add website to plugin.yml

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -98,7 +98,8 @@ tasks {
                 "name" to rootProject.name,
                 "group" to rootProject.group,
                 "version" to rootProject.version,
-                "description" to rootProject.description
+                "description" to rootProject.description,
+                "website" to "https://modrinth.com/plugin/crazyenvoys"
             )
         }
     }
